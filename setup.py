8fdadf63bfe200117260a4746fcfc473770d6832
miguel <miguel.varelaramos@braincreators.com>
2019-04-19 11:14:11 +0200

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name="open-image-tools",
+    name="open-images-tools",
     version='0.0.1',
     author="Braincreators B.V",
     author_email="miguel@braincreators.com",
