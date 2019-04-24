28ef08f72bdb2129cc0e13bf4c34d9f6e68fb00b
Tim Bakker <tim.bakker@braincreators.com>
2019-04-24 14:49:17 +0200

    extra sanity checks + printing of problematic annotation when assert fails

--- open_images_tools/coco/sanity_check.py
+++ open_images_tools/coco/sanity_check.py
@@ -14,32 +14,41 @@ def sanity_check(coco_data):
 
     seen_annotation_ids = set()
     for annotation in tqdm(coco_data['annotations'], total=len(coco_data['annotations'])):
-        # check ids
-        assert isinstance(annotation['id'], int)
-        assert annotation['id'] not in seen_annotation_ids
-        seen_annotation_ids.add(annotation['id'])
+        try:
+            # check ids
+            assert isinstance(annotation['id'], int)
+            assert annotation['id'] not in seen_annotation_ids
+            seen_annotation_ids.add(annotation['id'])
 
-        # check if image id exists
-        assert isinstance(annotation['image_id'], int)
-        assert annotation['image_id'] in image_id_to_idx
+            # check if image id exists
+            assert isinstance(annotation['image_id'], int)
+            assert annotation['image_id'] in image_id_to_idx
 
-        # check if category id exists
-        assert isinstance(annotation['category_id'], int)
-        assert annotation["category_id"] in valid_category_ids
+            # check if category id exists
+            assert isinstance(annotation['category_id'], int)
+            assert annotation["category_id"] in valid_category_ids
 
-        # check bbox
-        assert len(annotation['bbox']) == 4
-        assert all(isinstance(coord, int) for coord in annotation['bbox'])
-        assert all(coord >= 0 for coord in annotation['bbox'])
-        x, y, w, h = annotation['bbox']
+            # check bbox
+            assert len(annotation['bbox']) == 4
+            assert all(isinstance(coord, int) for coord in annotation['bbox'])
+            assert all(coord >= 0 for coord in annotation['bbox'])
+            x, y, w, h = annotation['bbox']
 
-        assert isinstance(annotation['area'], (int, float))
-        assert annotation['area'] == w * h, "area inconsistency found"
-        assert x + w <= coco_data['images'][image_id_to_idx[annotation['image_id']]]['width']
-        assert y + h <= coco_data['images'][image_id_to_idx[annotation['image_id']]]['height']
+            assert isinstance(annotation['area'], (int, float))
+            assert annotation['area'] == w * h, "area inconsistency found"
 
-        assert isinstance(annotation['iscrowd'], int)
-        assert annotation['iscrowd'] in {0, 1}
+            assert x < coco_data['images'][image_id_to_idx[annotation['image_id']]]['width']
+            assert y < coco_data['images'][image_id_to_idx[annotation['image_id']]]['height']
+            assert x + w <= coco_data['images'][image_id_to_idx[annotation['image_id']]]['width']
+            assert y + h <= coco_data['images'][image_id_to_idx[annotation['image_id']]]['height']
+
+            assert isinstance(annotation['iscrowd'], int)
+            assert annotation['iscrowd'] in {0, 1}
+        except AssertionError as e:
+            from pprint import pprint
+            print("Assertion failed in annotation:")
+            pprint(annotation)
+            raise e
 
 
 @click.command()
